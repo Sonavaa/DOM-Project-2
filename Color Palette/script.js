509584c271881eb90c878ec825c0e638b27755c0
Sonava <sonava@code.edu.az>
2023-12-21 10:16:42 +0400

    Color Palette added

---
+++ Color Palette/script.js
@@ -0,0 +1,9 @@
+const colors = document.querySelectorAll(".color");
+colors.forEach((color) => {
+    color.addEventListener("mouseover", () => {
+        color.classList.add("active");
+      });
+      color.addEventListener("mouseleave", () => {
+        color.classList.remove("active");
+      });
+})
